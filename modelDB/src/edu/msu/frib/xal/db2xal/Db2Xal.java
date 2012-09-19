c76f3e94ea854233ebd15734ad04c3c52f83e631
paul <paul@Dolphin>
2012-09-18 20:58:10 -0400

    test

--- modelDB/src/edu/msu/frib/xal/db2xal/Db2Xal.java
+++ modelDB/src/edu/msu/frib/xal/db2xal/Db2Xal.java
@@ -10,7 +10,12 @@ package edu.msu.frib.xal.db2xal;
  */
 public class Db2Xal {
     
-        /**
+    public void write2XDXF() {
+        StringBuilder sb = new StringBuilder("<?xml version='1.0'>");
+        sb.append("");
+    }
+    
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
