b6acf03a21287e6b114279e746c17c8278c4fbb5
chu <chu@Chu-w7lap.intranet.nscl.msu.edu>
2012-10-19 17:25:24 -0400

    EJB

---
+++ modelDB/src/ejbTest/NewSessionBeanRemote.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejbTest;
+
+import javax.ejb.Remote;
+
+/**
+ *
+ * @author chu
+ */
+@Remote
+public interface NewSessionBeanRemote {
+    
+}
