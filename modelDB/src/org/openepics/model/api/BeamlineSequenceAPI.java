bcf74018b0ffd39bb6dd69ad191dbd6e17bbf89b
chu <chu@Chu-w7lap.intranet.nscl.msu.edu>
2013-06-19 13:50:11 -0400

    Update.  Left element_install_device table out and now add back.

--- modelDB/src/org/openepics/model/api/BeamlineSequenceAPI.java
+++ modelDB/src/org/openepics/model/api/BeamlineSequenceAPI.java
@@ -31,7 +31,7 @@ import org.tools.persistence.PersistenceTools;
 public class BeamlineSequenceAPI {
 
     @PersistenceUnit
-    static Map properties=PersistenceTools.setPersistenceParameters("mysql", "localhost:3306", "root", "826529");
+    static Map properties=PersistenceTools.setPersistenceParameters("mysql", "localhost:3306", "paul", "model_test");
     static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("modelAPIPU",properties);
     static final EntityManager em = emf.createEntityManager();
 
